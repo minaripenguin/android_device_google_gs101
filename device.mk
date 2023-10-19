0b4cba36a136d8df8fef19a135272710d9c87088
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-19 08:33:18 +0800

    gs101: Fully enable Multi-Gen LRU
    Change-Id: I80a15cbbbad90c693b6c31afb0fd60d3bc86d031
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -1135,3 +1135,6 @@ PRODUCT_VENDOR_PROPERTIES += ro.crypto.metadata_init_delete_all_keys.enabled=tru
 
 # Hardware Info Collection
 include hardware/google/pixel/HardwareInfo/HardwareInfo.mk
+
+# Memory
+PRODUCT_PRODUCT_PROPERTIES += persist.device_config.mglru_native.lru_gen_config=all
